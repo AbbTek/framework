2fc732193f54d0388a4ddbb25f2d8699837530c5
abbtek <abbtek@gmail.com>
2014-03-16 01:38:12 -0400

    Primera versión

---
+++ Simplelabs.Framework/Configuration/Elements/ElementsCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplelabs.Framework.Configuration.Elements
+{
+    /// <summary>
+    /// Utilidad para manejar colecciones de elementos de configuración
+    /// </summary>
+    /// <typeparam name="T">Elemento de la configuración</typeparam>
+    public class ElementsCollection<T> : ConfigurationElementCollection where T : ConfigurationElement, IKeyElement, new()
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return (ConfigurationElement)new T();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            var obj = element as IKeyElement;
+            return obj != null ? obj.Key : null;
+        }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.AddRemoveClearMap;
+            }
+        }        
+    }
+}
---
+++ Simplelabs.Framework/Configuration/Elements/IKeyElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplelabs.Framework.Configuration.Elements
+{
+    /// <summary>
+    /// Para los coleciones genéricas
+    /// </summary>
+    public interface IKeyElement
+    {
+        /// <summary>
+        /// Representa el elemento que es la llave del objeto
+        /// </summary>
+        object Key
+        {
+            get;
+        }
+    }
+}
